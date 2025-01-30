9434acd0c2d581e8bfaacefd643fa9a0bbca33c1
LucaVallazza <Luvallazza@gmail.com>
2025-01-30 15:20:19 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { Music } from 'lucide-react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div className='flex flex-row'>
+      <Music height={40} width={40} className='my-auto mr-3' />
+      <h1 className=''>Music Bands App</h1>
+
+    </div>
+    </>
+  )
+}
+
+export default App
