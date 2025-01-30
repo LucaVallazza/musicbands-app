ed31ef145e7e96d073d3d960890918aaac2df76c
LucaVallazza <Luvallazza@gmail.com>
2025-01-30 15:36:09 -0300

    add: configured react-router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router";
+import LogInPage from "./pages/login-page.tsx";
+import BandsPage from "./pages/bands-page.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <Routes> 
+        <Route path = '/' element = {<Navigate to = {'/login'}></Navigate>}></Route>
+        <Route path = '/login' element= {<LogInPage />}></Route>
+        <Route path = '/bands' element= {<BandsPage />}></Route>
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
