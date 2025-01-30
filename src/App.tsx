9efed74d3764c4487fb81bd866fdbbe198e8f3da
LucaVallazza <Luvallazza@gmail.com>
2025-01-30 20:55:12 -0300

    add: react router config

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
-import { Music } from 'lucide-react'
 import './App.css'
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router'
+import LogInPage from './pages/login-page'
+import BandsPage from './pages/bands-page'
 
 function App() {
 
   return (
     <>
-    <div className='flex flex-row'>
-      <Music height={40} width={40} className='my-auto mr-3' />
-      <h1 className=''>Music Bands App</h1>
-
-    </div>
+    <BrowserRouter>
+      <Routes> 
+        <Route path = '/' element = {<Navigate to = {'/login'}></Navigate>}></Route>
+        <Route path = '/login' element= {<LogInPage />}></Route>
+        <Route path = '/bands' element= {<BandsPage />}></Route>
+      </Routes>
+    </BrowserRouter>
     </>
   )
 }
