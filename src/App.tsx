fc02cdcc8de66c1780fcd653bfe7965c2b6e0419
LucaVallazza <Luvallazza@gmail.com>
2025-01-31 14:27:56 -0300

    add: mock auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,20 @@
-import './App.css'
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router'
-import LogInPage from './pages/login-page'
-import BandsPage from './pages/bands-page'
+import "./App.css";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router";
+import LogInPage from "./pages/login-page";
+import BandsPage from "./pages/bands-page";
 
 function App() {
-
   return (
     <>
-    <BrowserRouter>
-      <Routes> 
-        <Route path = '/' element = {<Navigate to = {'/login'}></Navigate>}></Route>
-        <Route path = '/login' element= {<LogInPage />}></Route>
-        <Route path = '/bands' element= {<BandsPage />}></Route>
-      </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<LogInPage />}></Route>
+            <Route path="/bands" element={<BandsPage />}></Route>
+          <Route path="*" element={<Navigate to={"/bands"}></Navigate>}></Route>
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/pages/bands-page.tsx
+++ src/pages/bands-page.tsx
@@ -1,11 +1,24 @@
+import ProtectedRoute from "@/components/auth/protected-route";
+import { Button } from "@/components/ui/button";
+import { useNavigate } from "react-router";
+
 const BandsPage = () => {
-  return ( 
-    <div>
-      <h1>
-        Bands page
-      </h1>
-    </div>
-   );
-}
- 
+
+  const navigate = useNavigate()
+
+  const logout = (e) =>{
+    localStorage.removeItem('user')
+    navigate('/login')
+  }
+
+  return (
+    <ProtectedRoute>
+      <div>
+        <h1>Bands page</h1>
+        <Button onClick={logout}> Logout</Button>
+      </div>
+    </ProtectedRoute>
+  );
+};
+
 export default BandsPage;
