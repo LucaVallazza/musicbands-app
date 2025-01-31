a5e08060b2f9f141155ee08d9d09e25de18c4331
LucaVallazza <Luvallazza@gmail.com>
2025-01-31 20:37:05 -0300

    🎨 styles improvement

--- src/pages/band-details-page.tsx
+++ src/pages/band-details-page.tsx
@@ -1,5 +1,6 @@
 import { AppContext } from "@/App";
 import Header from "@/components/header";
+import { Card, CardContent } from "@/components/ui/card";
 import { Album, Band } from "@/lib/types";
 import { getGenreString } from "@/lib/utils";
 import axios from "axios";
@@ -46,31 +47,42 @@ const BandDetailPage = () => {
   return (
     <div className="w-full h-screen flex-col flex items-center text-left">
       <Header></Header>
+
       <div className=" w-3/4 xl:w-1/2 flex-col flex items-center">
-        <div className="mr-auto ml-0 bg-gray-200 p-5 shadow-md mt-10">
-          <Music size={70} color="#999"></Music>
-        </div>
-        {/* Name and year */}
-        <div className="w-full mt-5">
-          <h1 className="text-2xl font-bold">{band?.name}</h1>
-          <p className="text-gray-500 text-sm">
-            {getGenreString(band?.genreCode, genres)} • Formed in {band?.year}{" "}
-          </p>
-        </div>
+        <Card className="w-full m-0 p-4 mt-10">
+          <div className="w-fit">
+            <div className="mr-auto ml-0 bg-gray-200 p-5 shadow-md">
+              <Music size={70} color="#999"></Music>
+            </div>
+          </div>
+          {/* Name and year */}
+          <div className="w-full mt-5">
+            <h1 className="text-2xl font-bold">{band?.name}</h1>
+            <p className="text-gray-500 text-sm">
+              {getGenreString(band?.genreCode, genres)} • Formed in {band?.year}{" "}
+            </p>
+          </div>
+        </Card>
 
         {/* Data */}
-        <div className="w-full flex flex-row">
+
+        <div className="w-full flex flex-row justify-between mt-5">
+
+          
+        <Card className="w-[49%] h-full flex-col flex items-start p-4">
           {/* Members */}
-          <div className="w-1/2  h-fit flex-col flex items-start">
-            <div className="flex flex-row mt-10">
+          <div className="">
+            <div className="flex flex-row">
               <Users size={20} className="my-0 self-center mr-2"></Users>
               <h2 className="text-lg font-semibold">Members</h2>
             </div>
-            <div className="flex h-fit flex-col h-10 pt-4 gap-4">
+            <div className="flex h-fit flex-col pt-4 gap-4">
               {band?.members.map((member) => (
                 <div className="flex flex-row">
-                  <div className="bg-gray-200 mr-4 p-2 rounded-3xl">
-                    <User size={20} color="#999"></User>
+                  <div>
+                    <div className="bg-gray-200  mr-4 p-2 rounded-3xl">
+                      <User size={20} color="#999"></User>
+                    </div>
                   </div>
 
                   <div className="self-center font-semibold">{member.name}</div>
@@ -78,11 +90,16 @@ const BandDetailPage = () => {
               ))}
             </div>
           </div>
+          </Card>
+
+
+          
 
           {/* Albums */}
 
-          <div className="w-1/2  h-fit flex-col flex items-start">
-            <div className="flex flex-row mt-10">
+          <Card className="w-[49%] h-full flex-col flex items-start p-4">
+
+          <div className="flex flex-row selfcen ">
               <DiscAlbum
                 size={20}
                 className="my-0 self-center mr-2"
@@ -92,7 +109,7 @@ const BandDetailPage = () => {
             <div className="flex h-fit flex-col pt-4 gap-4">
               {albums?.map((album) => (
                 <div className="flex flex-row">
-                  <div>
+                  <div className="self-center">
                     <div className="bg-gray-200  mr-4 p-2 rounded-3xl">
                       <Disc size={20} color="#999"></Disc>
                     </div>
@@ -105,7 +122,9 @@ const BandDetailPage = () => {
                 </div>
               ))}
             </div>
-          </div>
+   
+
+          </Card>
         </div>
       </div>
     </div>
