b337a95359f50d2837243a19c4a22d98f198ab19
LucaVallazza <Luvallazza@gmail.com>
2025-01-31 16:11:29 -0300

    get and show bands from json server

---
+++ src/components/band-item.tsx
@@ -0,0 +1,33 @@
+import { Band } from "@/lib/types";
+import { Music } from "lucide-react";
+
+interface IProps{
+  band : Band,
+  genre: string | undefined
+}
+
+const BandItem = ({band, genre} : IProps) => {
+  
+  return ( 
+    <div className="w-3/4 flex flex-row text-left shadow-sm border-gray-50 border">
+    <div className="h-fit w-fit p-4">
+      <div className="bg-gray-100 p-2 rounded-3xl">
+      {/* <span style={{fontFamily : 'BarlowItalic'}} className="text-2xl" >
+        {Math.floor(band.year / 10) * 10 % 100}'s
+      </span> */}
+        <Music size={30}></Music>
+      </div>
+    </div>
+    <div className="h-auto justify-center flex-col flex w-full ">
+      <h2 className="text-xl w-fit   font-semibold">{band.name}</h2>
+      <p className="w-fit text-gray-400 text-sm">
+        {genre} - {band.country}, {band.year}
+      </p>
+      <p className="w-fit text-gray-400 text-sm"></p>
+    </div>
+  </div>
+
+   );
+}
+ 
+export default BandItem;
--- src/pages/bands-page.tsx
+++ src/pages/bands-page.tsx
@@ -1,21 +1,80 @@
-import ProtectedRoute from "@/components/auth/protected-route";
-import { Button } from "@/components/ui/button";
+import Header from "@/components/header";
+import { Band, Genre } from "@/lib/types";
+import axios from "axios";
+import { Music } from "lucide-react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router";
+import {
+  Guitar,
+  Radio,
+  Flame,
+  Gavel,
+  Skull,
+  Sword,
+  Moon,
+  Infinity,
+  Cross,
+} from "lucide-react";
+import BandItem from "@/components/band-item";
 
 const BandsPage = () => {
+  const navigate = useNavigate();
 
-  const navigate = useNavigate()
+  const [bands, setBands] = useState<Band[]>([]);
+  const [genres, setGenres] = useState<Genre[]>([]);
 
-  const logout = (e) =>{
-    localStorage.removeItem('user')
-    navigate('/login')
-  }
+  const genresIcons = {
+    rock: <Guitar size={30} />,
+    "rock-roll": <Radio size={30} />,
+    "hard-rock": <Flame size={30} />,
+    grunge: <Gavel size={30} />,
+    "heavy-metal": <Skull size={30} />,
+    "power-metal": <Sword size={30} />,
+    "black-metal": <Moon size={30} />,
+    "progressive-metal": <Infinity size={30} />,
+    "goth-metal": <Cross size={30} />,
+  };
+
+  useEffect(() => {
+    getBands();
+    getGenres();
+
+    return () => {};
+  }, []);
+
+  const getBands = async () => {
+    await axios.get("http://localhost:3000/bands").then((req) => {
+      if (req.data) {
+        console.log(req);
+        setBands(req.data);
+      }
+    });
+  };
+
+  const getGenres = async () => {
+    await axios.get("http://localhost:3000/genre").then((req) => {
+      if (req.data) {
+        console.log(req);
+        setGenres(req.data);
+      }
+    });
+  };
 
   return (
-      <div>
-        <h1>Bands page</h1>
-        <Button onClick={logout}> Logout</Button>
+    <div className="w-screen h-screen">
+      <Header />
+      <div className="w-full flex flex-col items-center m-0 p-0 gap-2">
+        {bands.length > 0 ? (
+          bands.map((band) => {
+            return (
+              <BandItem band={band} genre={genres.find((g) => g.code == band.genreCode)?.name} />
+            );
+          })
+        ) : (
+          <h2 className="text-2xl m-10">No bands to show</h2>
+        )}
       </div>
+    </div>
   );
 };
 
