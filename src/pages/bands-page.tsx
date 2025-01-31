bb50370c965443cc8fd78059dc241febd538f30e
LucaVallazza <Luvallazza@gmail.com>
2025-01-31 18:43:41 -0300

    removed unused imports and fix styling

--- src/pages/bands-page.tsx
+++ src/pages/bands-page.tsx
@@ -1,8 +1,7 @@
 import Header from "@/components/header";
 import { Band, Genre } from "@/lib/types";
 import axios from "axios";
-import { AccessibilityIcon, Filter, Music, Search, SortAsc, SortDesc } from "lucide-react";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router";
 
 import BandItem from "@/components/band-item";
@@ -14,7 +13,6 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
-import { Button } from "@/components/ui/button";
 
 const BandsPage = () => {
   const navigate = useNavigate();
@@ -22,12 +20,10 @@ const BandsPage = () => {
   const [bands, setBands] = useState<Band[]>([]);
   const [filteredBands, setFilteredBands] = useState<Band[]>([]);
   const [genres, setGenres] = useState<Genre[]>([]);
-  
-  const [ascOrder , setAscOrder] = useState<Boolean>(true)
-  const [filterBandName , setfilterBandName] = useState<string>("")
-  const [filterBandGenre , setfilterBandGenre] = useState<string>("")
-  const [filterBandDateOrder , setfilterBandDateOrder] = useState<string>("")
 
+  const [filterBandName, setfilterBandName] = useState<string>("");
+  const [filterBandGenre, setfilterBandGenre] = useState<string>("");
+  const [filterBandDateOrder, setfilterBandDateOrder] = useState<string>("");
 
   useEffect(() => {
     getBands();
@@ -41,7 +37,7 @@ const BandsPage = () => {
       if (req.data) {
         console.log(req);
         setBands(req.data);
-        setFilteredBands(req.data)
+        setFilteredBands(req.data);
       }
     });
   };
@@ -57,83 +53,79 @@ const BandsPage = () => {
 
   // const filter = () =>{
   //   e.preventDefault()
-    
+
   //   bands.find(band => (band.name == filterBandName))
 
   // }
 
   useEffect(() => {
-    let newBands = bands
-    if(filterBandName){
-      newBands = newBands.filter(band => (band.name.toLowerCase().includes(filterBandName.toLowerCase())))
+    let newBands = bands;
+    if (filterBandName) {
+      newBands = newBands.filter((band) =>
+        band.name.toLowerCase().includes(filterBandName.toLowerCase())
+      );
     }
-    
 
-    if(filterBandGenre != 'none' && filterBandGenre){
-      newBands = newBands.filter(band => (band.genreCode == filterBandGenre))
+    if (filterBandGenre != "none" && filterBandGenre) {
+      newBands = newBands.filter((band) => band.genreCode == filterBandGenre);
     }
-    
-    console.log(filterBandDateOrder)
-    if(filterBandDateOrder != "none" && filterBandDateOrder){
+
+    console.log(filterBandDateOrder);
+    if (filterBandDateOrder != "none" && filterBandDateOrder) {
       if (filterBandDateOrder == "newest") {
-        newBands = [...newBands].sort((a,b) => b.year - a.year)
-        
-      }
-      else{
+        newBands = [...newBands].sort((a, b) => b.year - a.year);
+      } else {
         // create a copy of the array with [...newBands] and then sort it to avoid refresh problems on react
-        newBands = [...newBands].sort((a,b) => a.year - b.year)
+        newBands = [...newBands].sort((a, b) => a.year - b.year);
       }
     }
-    
-    setFilteredBands(newBands)
 
+    setFilteredBands(newBands);
   }, [filterBandName, filterBandGenre, filterBandDateOrder]);
 
+  const getGenreString = (genre: string) => {
+    const _genre = genres.find((g) => g.code == genre)?.name;
 
-  const getGenreString = (genre : string) =>{
-    const _genre = genres.find((g) => g.code == genre)?.name 
-
-    return _genre ? _genre : genre
-  }
+    return _genre ? _genre : genre;
+  };
 
   return (
     <div className="w-full h-screen flex-col flex items-center">
       <Header />
       <div className="flex w-3/4 mt-3 flex-row sticky gap-2 top-0">
         <div className="flex-[5]">
-          <Input onChange={e => setfilterBandName(e.target.value)} placeholder="Name"></Input>
+          <Input
+            onChange={(e) => setfilterBandName(e.target.value)}
+            placeholder="Name"
+          ></Input>
         </div>
         <div className="flex-[3]">
-          <Select onValueChange={e => setfilterBandGenre(e)}>
+          <Select onValueChange={(e) => setfilterBandGenre(e)}>
             <SelectTrigger>
               <SelectValue placeholder="All genres"></SelectValue>
             </SelectTrigger>
-            <SelectContent >
-              <SelectItem value="none">
-                None
-              </SelectItem>
+            <SelectContent>
+              <SelectItem value="none">None</SelectItem>
               {genres.map((genre) => {
-                return <SelectItem key={genre.code} value={genre.code}>{genre.name}</SelectItem>;
+                return (
+                  <SelectItem key={genre.code} value={genre.code}>
+                    {genre.name}
+                  </SelectItem>
+                );
               })}
             </SelectContent>
           </Select>
         </div>
 
         <div className="flex-[1]">
-        <Select onValueChange={e => setfilterBandDateOrder(e)} >
+          <Select onValueChange={(e) => setfilterBandDateOrder(e)}>
             <SelectTrigger>
               <SelectValue placeholder="Date"></SelectValue>
             </SelectTrigger>
-            <SelectContent >
-              <SelectItem value="none">
-                None
-              </SelectItem>
-              <SelectItem value="newest">
-                Newest first
-              </SelectItem>
-              <SelectItem value="oldest">
-                Oldest first
-              </SelectItem>
+            <SelectContent>
+              <SelectItem value="none">None</SelectItem>
+              <SelectItem value="newest">Newest first</SelectItem>
+              <SelectItem value="oldest">Oldest first</SelectItem>
             </SelectContent>
           </Select>
         </div>
@@ -149,7 +141,7 @@ const BandsPage = () => {
               <BandItem
                 key={band.id}
                 band={band}
-                genre={ getGenreString(band.genreCode) }
+                genre={getGenreString(band.genreCode)}
               />
             );
           })
