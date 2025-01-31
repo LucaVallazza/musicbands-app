f8621dcbb2bf3d135e0a93cbcb64d394059b15c0
LucaVallazza <Luvallazza@gmail.com>
2025-01-31 17:24:05 -0300

    fixed styling

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -13,7 +13,7 @@ const Header = () => {
   };
 
   return (
-    <div className="flex  flex-row justify-between py-2 px-4 shadow-md">
+    <div className=" w-full flex  flex-row justify-between py-2 px-4 shadow-md">
       <div className="flex">
         <Music size={20} className="my-auto mr-2"></Music>
         <h1 className="text-2xl font-semibold">Music Band App</h1>
