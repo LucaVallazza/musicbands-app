dbb902f404cc33a7247592eb6fe0f19bf42db178
LucaVallazza <Luvallazza@gmail.com>
2025-01-30 22:04:25 -0300

    add: login screen (without auth)

--- src/pages/login-page.tsx
+++ src/pages/login-page.tsx
@@ -1,8 +1,40 @@
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { LogIn, Music } from "lucide-react";
+import { useNavigate } from "react-router";
+
 const LogInPage = () => {
+  
+  const navigate = useNavigate()
+
+  const logIn = (e : any) =>{
+    e.preventDefault()
+    console.log("mail: " +e.nativeEvent.srcElement[0].value)
+    navigate('/bands')
+  }
 
   return ( 
-    <div>
-      <h1>Login page</h1>
+    <div className="flex flex-col w-screen h-screen">
+      <Card className="m-auto w-96">
+        <CardHeader>
+          <div className="flex">
+            <Music size={20} className="my-auto mr-2"></Music>
+            <h1 className="text-2xl font-semibold">Music Band App</h1>
+
+          </div>
+        </CardHeader>
+        <CardContent>
+          <form onSubmit={e => logIn(e)} className="flex flex-col gap-2">
+            <Input placeholder="Email" type="email"></Input>
+            <Input placeholder="Password" type="password"></Input>
+            <Button className="mt-2">
+              Login
+              <LogIn></LogIn>  
+            </Button>
+          </form>
+        </CardContent>
+      </Card>
     </div>
    );
 }
